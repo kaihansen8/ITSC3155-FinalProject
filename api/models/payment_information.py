18abdff2c53852269813fd963908c9990bc67548
Kai_Hansen <khanse11@charlotte.edu>
2024-11-20 17:38:31 -0500

    feat: update payment information, I added ability for customer information to be null in case they wish not to give extra info

--- api/models/payment_information.py
+++ api/models/payment_information.py
@@ -8,8 +8,8 @@ class PaymentInformation(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_name = Column(String(100), ForeignKey('orders.customer_name'))
-    customer_email = Column(String(100))
-    customer_phone_number = Column(String(10))
-    customer_address = Column(String(300))
+    customer_email = Column(String(100), nullable=True)
+    customer_phone_number = Column(String(10), nullable=True)
+    customer_address = Column(String(300), nullable=True)
 
     orders = relationship('Order', backref='payment_information')
